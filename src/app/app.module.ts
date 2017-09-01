8d318801ed947de3a80d096b4b282d188bd2b141
Kelwin Lee <kelwin.lee@gmail.com>
2017-09-01 18:45:47 +0800

    Added transaction input, income list and expenses list components and UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransactionInputComponent } from './transaction-input/transaction-input.component';
+import { IncomeListComponent } from './income-list/income-list.component';
+import { ExpensesListComponent } from './expenses-list/expenses-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionInputComponent,
+    IncomeListComponent,
+    ExpensesListComponent
   ],
   imports: [
     BrowserModule
